56b369534acfad5ce6d23a0180d35081e12a2b55
lbmann <l.bmann@yahoo.com>
2021-03-16 00:38:47 -0400

    basic react routing and mobile first header styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Components/Header/Header';
+import Index from "./Components/Index/Index";
+import Projects from "./Components/Projects/Projects";
+import Contact from "./Components/Contact/Contact";
+import Footer from './Components/Footer/Footer';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Index} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "../../App.css";
+import "./Header.css";
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <div className="header">
+        <div className="h1-styling">
+            <Link to="/">
+                <div className="internal-flex-1">
+                    <img src="./images/headshot-cropped.png" alt="Laura Baumann headshot"/>
+                </div>
+                <div className="internal-flex-2">
+                    <h1>Laura Baumann</h1>
+                </div>
+            </Link>
+        </div>
+        <ul className="nav">
+            
+
+            <Link to="/projects">
+                <li>Projects</li>
+            </Link>
+
+            <Link to="/contact">
+                <li>Contact</li>
+            </Link>
+        </ul>
+    </div>
+  )
+}
