1ec65fdab0ef322c9b14cc7017f82e7b38c33037
lbmann <l.bmann@yahoo.com>
2021-03-16 16:31:59 -0400

    changing browserrouter to hashrouter to prep for build

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Index from "./Components/Index/Index";
 import Portfolio from "./Components/Portfolio/Portfolio";
 import Contact from "./Components/Contact/Contact";
 import Footer from './Components/Footer/Footer';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
